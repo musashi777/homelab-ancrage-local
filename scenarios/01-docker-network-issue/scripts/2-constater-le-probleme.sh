e162b3a8e5055398fa5de6abfbdaa23108341b04
uniatowitz <53368920+musashi777@users.noreply.github.com>
2025-10-16 02:16:04 +0200

    fix: Correct service name in script 2

--- scenarios/01-docker-network-issue/scripts/2-constater-le-probleme.sh
+++ scenarios/01-docker-network-issue/scripts/2-constater-le-probleme.sh
@@ -3,4 +3,4 @@ echo "Tentative de connexion au site web..."
 curl -I http://localhost:8080
 echo ""
 echo "Affichage des logs du proxy Nginx..."
-docker-compose logs nginx
+docker-compose logs proxy
